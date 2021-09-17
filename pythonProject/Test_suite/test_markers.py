9ad3abb2caa3535f97f903eb373daa1154888f68
jydozki <31565967+jydozki@users.noreply.github.com>
2021-09-17 14:46:59 +0200

    minor fix

--- pythonProject/Test_suite/test_markers.py
+++ pythonProject/Test_suite/test_markers.py
@@ -1,11 +1,15 @@
 import pytest
+
+
 @pytest.mark.smoke
 def test_login():
     print("login done")
 
+
 @pytest.mark.regression
 def test_regression():
     print("regression done")
+
 
 @pytest.mark.smoke
 def test_logout():

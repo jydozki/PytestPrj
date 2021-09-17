85682deac747f4a64c31daa6a1863b16d05f297d
jydozki <31565967+jydozki@users.noreply.github.com>
2021-09-17 10:05:12 +0200

    fix paycheck error

--- pythonProject/Test_suite/math_func.py
+++ pythonProject/Test_suite/math_func.py
@@ -1,11 +1,10 @@
-# Thisbonus=Nonele Python script.
 def add(x, y):
     return x + y
 
 def product(x, y):
     return x * y
 
-def paycheck(salary=50000, bonus=200):
+def paycheck(salary=50000, bonus=2000):
     paycheck = (float(salary) + float(bonus))
     return paycheck
 
--- pythonProject/Test_suite/test_markers.py
+++ pythonProject/Test_suite/test_markers.py
@@ -1,20 +1,12 @@
 import pytest
-import allure
-
-@allure.description ("validate login")
-@allure.severity (severity_level="CRITICAL")
 @pytest.mark.smoke
 def test_login():
     print("login done")
 
-@allure.description("validate regression passed")
-@allure.severity (severity_level="HIGH")
 @pytest.mark.regression
 def test_regression():
     print("regression done")
 
-@allure.description ("validate logout")
-@allure.severity (severity_level="NORMAL")
 @pytest.mark.smoke
 def test_logout():
     print("logout done")
